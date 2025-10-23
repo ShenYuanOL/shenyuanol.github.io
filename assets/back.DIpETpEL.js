d0f20f0fb5364751243683482bf6c58ed3aa910f
ShenYuanOL <ShenYuanOL@users.noreply.github.com>
2025-10-23 17:33:06 +0000

    deploy: 1b3625b1d5da2fbe82a114376bc961a13232d7f5

---
+++ assets/back.DIpETpEL.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunks/vue-router.C4vf3l4V.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
